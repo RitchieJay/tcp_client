ea846fd66d0ab47e1e077789d256837c4e58444f
RitchieJay <75122521+RitchieJay@users.noreply.github.com>
2023-04-14 18:37:42 +0100

    Initial Commit

---
+++ tcp_client.py
@@ -0,0 +1,23 @@
+# import library for networking
+import socket
+
+# destination host
+target_host = "www.google.com"
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host,target_port))
+
+# encode some data
+data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+
+# send some data
+client.send(data.encode())
+
+# receive some data
+response = client.recv(4096)
+
+print (response.decode())
